3722d1a735558c3b04f968b84fba36e59eca9a83
Ivan Raikov <ivan.g.raikov@gmail.com>
2024-08-09 12:36:03 -0700

    Fixes to initial AAC config

---
+++ jobscripts/netclamp_go_PYR.sh
@@ -0,0 +1,16 @@
+export DATA_PREFIX=./datasets
+
+mpirun -n 1 network-clamp go  \
+       -c Network_Clamp_PYR_gid_48041.yaml \
+       --template-paths templates --dt 0.01 \
+       -p PYR -g 48041  -t 5000 \
+       --recording-profile "Network clamp exc synaptic" \
+       --dataset-prefix $DATA_PREFIX \
+       --config-prefix config \
+       --arena-id A --stimulus-id Diag \
+       --input-features-path $DATA_PREFIX/Full_Scale/Full_Scale_CA1_input_features.h5 \
+       --input-features-namespaces 'Constant Selectivity' \
+       --results-path results/netclamp
+
+
+

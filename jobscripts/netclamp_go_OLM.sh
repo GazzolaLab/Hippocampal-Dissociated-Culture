5eba4c0b317356897df12c51fc5ce50364c426f4
Ivan Raikov <ivan.g.raikov@gmail.com>
2024-08-10 11:14:00 -0700

    updated OLM config

---
+++ jobscripts/netclamp_go_OLM.sh
@@ -0,0 +1,16 @@
+export DATA_PREFIX=./datasets
+
+mpirun -n 1 network-clamp go  \
+       -c Network_Clamp_OLM_gid_337610.yaml \
+       --template-paths templates --dt 0.01 \
+       -p OLM -g 337610  -t 5000 \
+       --recording-profile "Network clamp exc synaptic" \
+       --dataset-prefix $DATA_PREFIX \
+       --config-prefix config \
+       --arena-id A --stimulus-id Diag \
+       --input-features-path $DATA_PREFIX/Full_Scale/Full_Scale_CA1_input_features.h5 \
+       --input-features-namespaces 'Constant Selectivity' \
+       --results-path results/netclamp
+
+
+

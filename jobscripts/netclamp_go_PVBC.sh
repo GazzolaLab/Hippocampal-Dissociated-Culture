3000e8667b1ad40c2999da46fea25b7a6ad78e73
Ivan Raikov <ivan.g.raikov@gmail.com>
2024-08-11 22:02:53 -0700

    updates to PVBC configuration

---
+++ jobscripts/netclamp_go_PVBC.sh
@@ -0,0 +1,16 @@
+export DATA_PREFIX=./datasets
+
+mpirun -n 1 network-clamp go  \
+       -c Network_Clamp_PVBC_gid_313500.yaml \
+       --template-paths templates --dt 0.01 \
+       -p PVBC -g 313500  -t 5000 \
+       --recording-profile "Network clamp inh synaptic" \
+       --dataset-prefix $DATA_PREFIX \
+       --config-prefix config \
+       --arena-id A --stimulus-id Diag \
+       --input-features-path $DATA_PREFIX/Full_Scale/Full_Scale_CA1_input_features.h5 \
+       --input-features-namespaces 'Constant Selectivity' \
+       --results-path results/netclamp
+
+
+
